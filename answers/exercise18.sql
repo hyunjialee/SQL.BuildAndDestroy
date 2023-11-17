f7a06c4c49bba09a0c7f4c965e55f3eea1a08493
hyunjialee <hyjialee@gmail.com>
2023-11-17 15:51:47 -0500

    Finished

---
+++ answers/exercise18.sql
@@ -0,0 +1,1 @@
+UPDATE Student SET City = 'Edinburgh' SET Country = 'Scotland' WHERE ID = 35;
---
+++ answers/exercise8.sql
@@ -0,0 +1,1 @@
+INSERT INTO STUDENTS (StudentName, Address, City, PostalCode, Country) VALUES ('Jane Doe', '57 Union St', 'Glasgow, Scotland', 'G13RB');
